2f081f0d5eaf6137605364275bbc4b22314d7f9f
Manuel Martinez <33674827+alaskanbear48@users.noreply.github.com>
2020-06-11 14:57:02 -0500

    Add files via upload

---
+++ Project 06/Island.h
@@ -0,0 +1,195 @@
+//The purpose of this program was to create an adjacency list using classes. Structs were not used 
+//in order to keep the program class focused. The program has 5 classes. However MyStringNode and MyStringList
+//are both used for reading text files and preventing infinite loops. Thus they work similar to MyNode and MyList.
+//First the Island class creates an adjacency list for a specific island. It uses both the MyList and MyNode class.
+//MyList manipulates the adjacency list using the MyNode class. Since structs are not used the MyNode class is used instead
+//to connect nodes and set elements. All together the program creates adjacency lists for every island an a sole link list 
+//that stores text file names.
+
+//CS 211 Program 6 UIC <Manuel M Martinez> <mmart233>
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+
+//Node Class to create Nodes in a list 
+class MyNode
+{
+	private:
+		int Value;
+		MyNode* Next;
+
+	public:
+
+	//Constructor with initial value (Node connects to nothing) 
+	MyNode(int value);
+	
+	//Constructor with initial value (Node connects to another node)
+	MyNode(int value, MyNode* cur);
+
+	//Assign New value to the current Node
+	void setElem(int value);
+	
+	//Get the element of the current Node
+	int getElem();
+	
+	//Connect the Link
+	void setNext(MyNode* cur);
+
+	//Go to the next node in the link list (Change current node)
+	MyNode* getNext();
+
+};
+
+//Node Class to create Nodes in a char List 
+class MyStringNode
+{
+	private:
+		char Value[50];
+		MyStringNode* Next;
+
+	public:
+
+
+	//Constructor with initial value (Node connects to nothing) 
+	MyStringNode(char* value);
+	
+	//Constructor with initial value (Node connects to another node)
+	MyStringNode(char* value, MyStringNode* cur);
+
+	//Assign New value to the current Node
+	void setElem(char* value);
+	
+	//Get the element of the current Node
+	char* getElem();
+	
+	//Connect the Link
+	void setNext(MyStringNode* cur);
+
+	//Go to the next node in the link list (Change current node)
+	MyStringNode* getNext();
+
+};
+
+//Class for creating a link list 
+class MyList
+{
+	private:
+		MyNode* Head;
+	
+	public:
+
+//Initializes Head Node as NULL
+MyList();
+
+//Shows all the values stored in each node 
+void show();
+
+//Insert New Node into List
+void insert(int value);
+
+//Removes a node from the link list 
+void Remove(int value);
+
+//Check if Link list is empty
+bool isEmpty();
+
+//Removes all the nodes in the Link List 
+void RemoveAll();
+	
+//Searches for a value at a positon on the list 	
+int Search(int value);
+
+//Searches for a value on the list 
+int SearchVal(int value);
+
+//Returns the lenght of the list 
+int getLength();
+
+};
+
+//Class for creating a char link list 
+class MyStringList
+{
+	private:
+		MyStringNode* Head;
+	
+	public:
+
+//Copy constructor //Fix Later if needed
+//*MyList(const MyList& param)
+
+//Initializes Head Node as NULL
+MyStringList();
+
+//Shows all the values stored in each node 
+void show();
+
+//Insert New Node into List
+void insert(char* value);
+
+//Removes a node from the link list 
+void Remove(char* value);
+
+//Check if Link list is empty
+bool isEmpty();
+
+//Removes all the nodes in the Link List 
+void RemoveAll();
+
+//Searches for a value
+bool Search(char* value);
+
+//Returns the lenght of the list 
+int getLength();
+
+};
+
+//Class to create a link list of edges to a specific island
+class Island
+{
+	private:
+		
+		//Create link list 
+		MyList Neighbors;
+		bool Visited;
+
+	public: 
+	
+	//Default constructor
+	Island();
+	
+	//Insert edge into link list
+	void insertAdjacent(int value);
+	
+	//Show all edges in link list
+	void showAdjacents();
+	
+	//Remove an edge from link list
+	void removeAdjacent(int value);
+	
+	//Remove every edge from link list
+	void removeAllAdj();
+	
+	//Check if link list is empty
+	bool isAdjEmpty();
+	
+	//Set if island has been visited
+	void setVisited(bool v);
+	
+	//Check if island has been visited 
+	bool isVisited();
+	
+	//Find value at a postion in the link list
+	int findIsland(int value);
+	
+	//Find a value in the link list 
+	int findAdjacent(int value);
+	
+	//Find the lenght of the link list
+	int findLength();
+	
+};
+
+
---
+++ Project 06/project6Base.cpp
@@ -0,0 +1,485 @@
+#include "Island.h"
+
+class ArchipelagoExpedition
+{
+ private:
+   //Data Members for the Archipelago Network here
+   Island* Darwin;	
+   int N;
+	
+   //Making a String Link List to store text files  	
+   MyStringList txtFiles;	
+  
+ public:
+  
+ //Use a constructor to initialize the Data Members for your expedition
+ ArchipelagoExpedition()
+ {
+	 //Set a starting size of 10 islands
+	 N = 11;
+	 Darwin = new Island[N];
+ }
+
+ //Resize helper function 	
+ void rezize(int n)
+ {
+	 //Set the new size
+	 N = n + 1;
+	 Darwin = new Island[N];
+ }
+  
+  
+ //The main loop for reading in input
+ void processCommandLoop (FILE* inFile)
+ {
+  char  buffer[300];
+  char* input;
+
+  input = fgets ( buffer, 300, inFile );   // get a line of input
+    
+  // loop until all lines are read from the input
+  while (input != NULL)
+  {
+    // process each line of input using the strtok functions 
+    char* command;
+    command = strtok(input, " \n\t");
+
+    printf ("*%s*\n", command);
+    
+    if ( command == NULL )
+      printf ("Blank Line\n");
+ 
+    else if ( strcmp (command, "q") == 0)//
+	{
+		
+	   //Delete the Island Link List 	
+	   for(int i = 0; i < N; i++)
+	   {
+		   if(!Darwin[i].isAdjEmpty())
+		   {
+			   Darwin[i].removeAllAdj();
+		   }
+	   
+	   }
+		
+	  delete[] Darwin;
+		
+	  //Delete the text file link list 	
+	  txtFiles.RemoveAll();
+		
+      exit(1);
+	}
+    else if ( strcmp (command, "?") == 0)// 
+      showCommands();
+     
+    else if ( strcmp (command, "t") == 0)//
+	{
+      doTravel();
+	  printf("\n");
+	}
+    else if ( strcmp (command, "r") == 0)//
+	{
+      doResize();
+	  printf("\n");
+	}
+    else if ( strcmp (command, "i") == 0)//
+	{
+      doInsert();
+	  printf("\n");
+	}
+    else if ( strcmp (command, "d") == 0)//
+	{
+      doDelete();
+	  printf("\n");
+	}
+    else if ( strcmp (command, "l") == 0)// 
+	{
+      doList();
+	  printf("\n");	
+	}
+    else if ( strcmp (command, "f") == 0)
+	{
+      doFile();
+	  printf("\n");
+	}
+    else if ( strcmp (command, "#") == 0)// 
+      ;
+     
+    else
+      printf ("Command is not known: %s\n", command);
+     
+	//Let the user know they can input another command   
+	printf ("Input next command\n");  
+    input = fgets (buffer, 300, inFile);   // get the next line of input
+
+  }
+ }
+
+ //DFS Helper function 	
+ void depthFirstSearchHelper(int x, int y)
+ {
+	 //Mark all islands as unvisited
+	 for(int i = 0; i < N; i++)
+	 {
+		 
+		 Darwin[i].setVisited(false);
+		 
+	 }
+	 
+	 if(dfs(x, y) == true)
+	 {
+		 printf("You can get from Island %d to Island %d in one or more ferry rides.\n", x, y);
+	 }
+	 else
+	 {
+		 printf("You can NOT get from Island %d to Island %d in one or more ferry rides.\n", x, y);
+	 }
+	 
+	 
+ }
+
+ //Depth First Search function
+ bool dfs(int a, int b)
+ {
+	int c = 0;
+	
+	//For every island c that can be reached from a in one ferry ride 
+	for(int i = 0; i < Darwin[a].findLength(); i++)
+	{
+		c = Darwin[a].findIsland(i);
+		
+		if(c == b)
+		{
+			return true;
+		}
+		
+		//If Island C is unvisited
+		if(!Darwin[c].isVisited())
+		{
+			//Mark Island C as visited
+			Darwin[c].setVisited(true);
+			
+			if(dfs(c, b) == true)
+			{
+				return true;
+			}
+		}
+			
+	}
+	return false;
+}
+	
+ //Display commands	
+ void showCommands()
+ {
+   printf ("The commands for this project are:\n");
+   printf ("  q \n");
+   printf ("  ? \n");
+   printf ("  # \n");
+   printf ("  t <int1> <int2> \n");
+   printf ("  r <int> \n");
+   printf ("  i <int1> <int2> \n");
+   printf ("  d <int1> <int2> \n");
+   printf ("  l \n");
+   printf ("  f <filename> \n");
+ }
+ 
+ //Check if it is possible to travel from point a to point b 
+ void doTravel()
+ {
+   int val1 = 0;
+   int val2 = 0;
+
+   // get an integer value from the input
+   char* next = strtok (NULL, " \n\t");
+ 
+   if ( next == NULL )
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val1 = atoi ( next );
+   if ( val1 == 0 && strcmp (next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   
+   // get another integer value from the input
+   next = strtok (NULL, " \n\t");
+
+   if ( next == NULL )
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val2 = atoi ( next );
+   if ( val2 == 0 && strcmp (next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   
+	 //Error checking. If no errors then DFS is used  
+	 if((val1 > N - 1 || val1 < 0) || (val2 > N - 1 || val2 < 0))
+	 {
+		 printf("One of the Islands does not exist.\n");
+	 }
+	 else if(Darwin[val1].isAdjEmpty() || Darwin[val2].isAdjEmpty())
+	 {
+		 printf("One of the Islands does not have edges, and cannot be reached.\n");
+	 }
+	 else
+	 {
+		printf("Performing the Travel Command from %d to %d\n", val1, val2);
+		depthFirstSearchHelper(val1, val2);
+	 }
+	  
+ }
+ 
+ //Resize Island array
+ void doResize()
+ {
+   int val1 = 0;
+   
+   //Get an integer value from the input
+   char* next = strtok(NULL, " \n\t");
+   
+   if (next == NULL)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val1 = atoi(next);
+   if (val1 == 0 && strcmp(next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }	 
+	//Check if the resize value is greater than 0
+	if(val1 == 0)
+	{
+		return;
+	}
+	 
+   printf ("Performing the Resize Command with new size of %d\n", val1);
+
+	 Island* tmp = Darwin;
+	 
+   //Delete all the values of the old Island Array 
+   for(int i = 0; i < N; i++)
+   {
+	   if(!tmp[i].isAdjEmpty())
+	   {
+		   tmp[i].removeAllAdj();
+	   }
+	   
+   }
+	 delete[] tmp;
+	 
+	 //Create a new array with the new size 
+	 rezize(val1);
+	 
+	  
+ }
+ 
+ //Insert edge to Island Array 
+ void doInsert()
+ {
+	int val1 = 0;
+    int val2 = 0;
+	 
+	 
+   //Get an integer value from the input
+   char* next = strtok(NULL, " \n\t");
+   
+   if (next == NULL)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val1 = atoi(next);
+   if (val1 == 0 && strcmp(next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+	 
+   //Get another integer value from the input
+   next = strtok(NULL, " \n\t");
+	 
+   if(next == NULL)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val2 = atoi(next);
+	 
+   if(val2 == 0 && strcmp(next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+	 
+	//Error checking. If it passes then the edge can be inserted 
+	if((val1 > N - 1 || val1 < 0) || (val2 > N - 1 || val2 < 0))
+	{
+		printf ("One Island does not exist.\n");
+	}
+	else if(Darwin[val1].findAdjacent(val2) == val2)
+	{
+		printf("Adjacent has already been inserted.\n");
+	}
+	else
+	{
+		
+	//Insert the edge into the link list for that specific Island
+	printf ("Performing the Insert Command with %d and %d\n", val1, val2);
+	Darwin[val1].insertAdjacent(val2);  
+	 
+	}
+ }
+ 
+ //Delete an edge from the Island Array 	
+ void doDelete()
+ {
+	int val1 = 0;
+    int val2 = 0;
+	 
+	 
+   //Get an integer value from the input
+   char* next = strtok(NULL, " \n\t");
+   
+   if (next == NULL)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val1 = atoi(next);
+   if (val1 == 0 && strcmp(next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+	 
+	 
+   //Get another integer value from the input
+   next = strtok(NULL, " \n\t");
+  
+   if(next == NULL)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+   val2 = atoi(next);
+   if(val2 == 0 && strcmp(next, "0") != 0)
+   {
+     printf ("Integer value expected\n");
+     return;
+   }
+	 
+	 //Error checking. If it passes then an Edge can be deleted 
+	 if((val1 > N - 1 || val1 < 0) || (val2 > N - 1 || val2 < 0))
+	 {
+		 printf ("An Island does not exist an cannot be deleted.\n");
+	 }
+	 else if(Darwin[val1].isAdjEmpty() || Darwin[val2].isAdjEmpty())
+	 {
+		 printf ("An Island cannot be reached and has no edges.\n");
+	 }
+	 else
+	 {
+		//Delete the edge into the link list for that specific Island
+		printf ("Performing the Delete Command with %d and %d\n", val1, val2);
+		Darwin[val1].removeAdjacent(val2); 
+	 }
+ }
+ 
+ //Show all the corresponding edges for every island in the array 
+ void doList()
+ {
+	 //Loop through every possible island 
+	 for(int i = 0; i < N; i++)
+	 {
+		 if(!Darwin[i].isAdjEmpty())
+		 {
+			 printf("Island %d has these edges:\n", i);
+			 Darwin[i].showAdjacents();
+		 }
+		 else
+		 {
+			 printf("Island %d is empty and has no edges.\n", i);
+		 }
+		  
+	 }
+	 
+ }
+ 
+ //Read commands from a txt file and open txt file  
+ void doFile()
+ {
+   // get a filename from the input
+   char* fname = strtok (NULL, " \n\t");
+   if ( fname == NULL )
+   {
+     printf ("Filename expected\n");
+     return;
+   }
+   
+   printf ("Performing the File command with file: %s\n", fname);
+   
+   
+     //Verify the file name is not currently in use
+     char a[50];  
+	 strncpy(a, fname, sizeof(a));
+	 
+	  if(txtFiles.Search(a))
+	   {
+		   printf ("File Has already been used.\n");
+		   return;
+	   }
+	   else
+	   {
+		   txtFiles.insert(a);
+	   }
+	 
+         //Open the file using fopen creating a new instance of FILE*
+		 FILE* inFile;
+		 inFile = fopen(fname, "r");
+	 
+	 
+         //Recursively call processCommandLoop() with this new instance of FILE* as the parameter
+         if(inFile == NULL)
+		 {
+			 printf("File does not exist.\n");
+			 return;
+		 }
+		 
+		  processCommandLoop(inFile);
+	 
+         //Close the file when processCommandLoop() returns
+         fclose(inFile);
+		  
+ }
+	
+};
+
+int main (int argc, char** argv)
+{
+  // set up the variable inFile to read from standard input
+  FILE* inFile = stdin;
+
+  // set up the data needed for the island adjcency list
+  ArchipelagoExpedition islandData;
+   
+  // call the method that reads and parses the input
+  islandData.showCommands();
+  printf ("\nEnter commands at blank line\n");
+  printf ("    (No prompts are given because of f command)\n");
+  islandData.processCommandLoop (inFile);
+   
+  printf ("Goodbye\n");
+  return 1;
+ }
+
+
